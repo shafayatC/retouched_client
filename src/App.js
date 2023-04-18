a88b5ad90a3a7d72fc6f882594d8ac0e1024a0aa
FaizulHassan-Rhine <faizulhassan.rien@gmail.com>
2023-04-18 14:26:39 +0600

    add new component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Home from './Components/Home/Home';
+import ImageUpload from './Components/ImageUpload/ImageUpload';
 
 function App() {
   return (
     <div className="App">
       <Home></Home>
+      <ImageUpload></ImageUpload>
     </div>
   );
 }
