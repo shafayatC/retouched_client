0fa1721bef3b07f46646842e9bede5aa64f25b49
FaizulHassan-Rhine <faizulhassan.rien@gmail.com>
2023-04-26 16:40:52 +0600

    design drag and drop

--- src/Components/ImageUpload/ImageUpload.js
+++ src/Components/ImageUpload/ImageUpload.js
@@ -256,8 +256,9 @@ const ImageUpload = () => {
                 multiple
             />
 
-            <div onDrop={dropHandler} onDragOver={dragOverHandler} id='drop-zone' className="bg-slate-400 w-96 h-96 mx-auto">
-                <p>Drage and drop</p>
+            <div onDrop={dropHandler} onDragOver={dragOverHandler} id='drop-zone' className="bg-[#A0A2B7] rounded-lg flex flex-col justify-center items-center w-[600px] h-96 mx-auto">
+                <p><i className="fa-solid text-7xl fa-download"></i></p>
+                <p className="text-center text-lg mt-4 ">Choose a <span className="font-bold">File</span> or drag it here...</p>
             </div>
 
             <div className="relative">
